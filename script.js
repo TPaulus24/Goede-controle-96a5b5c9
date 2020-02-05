30ec7a14b16fb71bd4935e62b77d9435f03fa61c
FIRST_TOM LAST_PAULUS <54597104+TPaulus24@users.noreply.github.com>
2020-02-05 15:26:46 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,33 @@
+let answers = ['Parijs', '8', 'Ijsselmeer']
+
+function controleer() {
+    let goed = 0;
+    let input1 = document.getElementById("vraag1").value;
+    if (input1 == answers[0]) {
+        document.getElementById("vraag1").style.backgroundColor = "#42ff6b";
+        goed += 1;
+    } else {
+        document.getElementById("vraag1").style.backgroundColor = "#ff4f42";
+    }
+
+    let input2 = document.getElementById("vraag2").value;
+    if (input2 == answers[1]) {
+        document.getElementById("vraag2").style.backgroundColor = "#42ff6b";
+        goed += 1;
+    } else {
+        document.getElementById("vraag2").style.backgroundColor = "#ff4f42";
+    }
+
+    let input3 = document.getElementById("vraag3").value;
+    if (input3 == answers[2]) {
+        document.getElementById("vraag3").style.backgroundColor = "#42ff6b";
+        goed += 1;
+    } else {
+        document.getElementById("vraag3").style.backgroundColor = "#ff4f42";
+    }
+    if (goed == 3) {
+        document.getElementById("goedeAntwoorden").innerHTML = "Alle antwoorden zijn goed!";
+    } else {
+        document.getElementById("goedeAntwoorden").innerHTML = goed + " van de 3 antwoorden zijn goed!";
+    }
+}
